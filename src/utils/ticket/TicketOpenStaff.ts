97e86e9aaff2bf6030a5f8c1f366d3d0ac8d8e90
Ziga <ziga.zajc007@gmail.com>
2023-06-20 22:15:58 +0200

    Update

--- src/utils/ticket/TicketOpenStaff.ts
+++ src/utils/ticket/TicketOpenStaff.ts
@@ -1,99 +1,91 @@
 import {TextChannel, Client, EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle, ChannelType, User} from 'discord.js';
 
 function openTicketStaff(staff: User, user: User, type: string, client: Client, channelName: string, interact) {
-    const guild = client.guilds.cache.get('1051758423211003951');
-    if (!guild) return;
+	const guild = client.guilds.cache.get('1051758423211003951');
+	if (!guild) return;
 
-    guild.channels.create({
-        name: channelName,
-        type: ChannelType.GuildText,
-        parent: null,
-        permissionOverwrites: [
-        {
-            id: guild.roles.everyone,
-            deny: ["ViewChannel"]
-        },
-        {
-            id: user.id,
-            allow: ["ViewChannel"]
-        },
-        {
-            id: staff.id,
-            allow: ["ViewChannel"],
-        }, {
+	guild.channels.create({
+		name: channelName,
+		type: ChannelType.GuildText,
+		parent: null,
+		permissionOverwrites: [{
+			id: guild.roles.everyone,
+			deny: ["ViewChannel"]
+		},{
+			id: user.id,
+			allow: ["ViewChannel"]
+		},{
+			id: staff.id,
+			allow: ["ViewChannel"],
+		},{
+			id: "1095773070414844076", // Team role ID
+			allow: ["ViewChannel"],
+		},{
+			id: "1106641978218782792", // Mod role ID
+			allow: ["ViewChannel"]
+		}]
+	}).then((channel) => {
 
-            id: "1095773070414844076", // Team role ID
-            allow: ["ViewChannel"],
+		if (channel instanceof TextChannel) {
 
-        },{
-            id: "1106641978218782792", // Mod role ID
-            allow: ["ViewChannel"]
-        }]
-    })
-        .then((channel) => {
+			const close = new ButtonBuilder()
+				.setCustomId('close_ticket')
+				.setLabel('Close')
+				.setStyle(ButtonStyle.Danger);
+			const claim = new ButtonBuilder()
+				.setCustomId('claim_ticket')
+				.setLabel('Claim')
+				.setStyle(ButtonStyle.Success);
 
-            if (channel instanceof TextChannel) {
+			const row = new ActionRowBuilder()
+				.addComponents(close, claim);
 
-                const close = new ButtonBuilder()
-                    .setCustomId('close_ticket')
-                    .setLabel('Close')
-                    .setStyle(ButtonStyle.Danger);
-                const claim = new ButtonBuilder()
-                    .setCustomId('claim_ticket')
-                    .setLabel('Claim')
-                    .setStyle(ButtonStyle.Success);
-
-                const row = new ActionRowBuilder()
-                    .addComponents(close, claim);
-
-                channel.send("Ticket created by <@"+staff.id+"> for the applicant <@"+user.id+">");
-                // @ts-ignore
-                channel.send({ components: [row],  embeds: [getEmbed(staff, user, type, client)]});
-                interact.reply({
-                    embeds: [
-                        new EmbedBuilder()
-                            .setColor(0x7ACB0C)
-                            .setTitle('Success!')
-                            .setDescription("Created ticket: <#" + channel.id + ">")
-                            .setTimestamp()
-                    ],
-                    ephemeral: true
-                });
-            }
-        })
-        .catch((error) => {
-            console.error('Error creating ticket channel:', error);
-            interact.reply({
-                embeds: [
-                    new EmbedBuilder()
-                        .setColor(0xFFFE00)
-                        .setTitle('Warning!')
-                        .setDescription("Something went wrong...")
-                        .setTimestamp()
-                ],
-                ephemeral: true
-            });
-        });
+			channel.send("Ticket created by <@"+staff.id+"> for the applicant <@"+user.id+">");
+			// @ts-ignore
+			channel.send({ components: [row],  embeds: [getEmbed(staff, user, type, client)]});
+			interact.reply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor(0x7ACB0C)
+						.setTitle('Success!')
+						.setDescription("Created ticket: <#" + channel.id + ">")
+						.setTimestamp()
+				],
+				ephemeral: true
+			});
+		}
+	}).catch((error) => {
+		console.error('Error creating ticket channel:', error);
+		interact.reply({
+			embeds: [
+				new EmbedBuilder()
+					.setColor(0xFFFE00)
+					.setTitle('Warning!')
+					.setDescription("Something went wrong...")
+					.setTimestamp()
+			],
+			ephemeral: true
+		});
+	});
 
 }
 
 function getEmbed(staff: User, user: User, type: string, client: Client) {
-    let embed;
-    switch (type) {
-        case "content": {
-            embed = new EmbedBuilder()
-                .setColor(0xEB8922)
-                .setTitle('Application Ticket!')
-                .setDescription("This ticket is about the content application of " + user.username)
-                .setTimestamp()
+	let embed;
+	switch (type) {
+		case "content": {
+			embed = new EmbedBuilder()
+				.setColor(0xEB8922)
+				.setTitle('Application Ticket!')
+				.setDescription("This ticket is about the content application of " + user.username)
+				.setTimestamp()
+			break;
+		}
+		default: {
 
-            break;
-        }
-        default: {
-
-        }
-    }
-    return embed;
+		}
+	}
+	return embed;
 }
 
 export { openTicketStaff }
